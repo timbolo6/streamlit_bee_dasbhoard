7c0b3693b185c19d181d6c8fe5662f7fde07a46a
Tim Wywiol <tim-leon.wywiol.5831@student.uu.se>
2024-09-24 21:35:11 +0200

    Refactor default date range in sidebar to display 7 days of data

--- app.py
+++ app.py
@@ -38,12 +38,12 @@ data['created_at'] = pd.to_datetime(data['created_at'])
 columns_to_plot = st.sidebar.multiselect(
     "Select the columns to plot",
     ['weight_cleaned', 'temperature', 'humidity'],
-    default=['weight_cleaned']  # Default column to display
+    default=['weight_cleaned','temperature'] 
 )
 
 # Default date range is the last day of data
 end_date = data['created_at'].max()
-start_date = end_date - timedelta(days=1)
+start_date = end_date - timedelta(days=7)  # Default date range is 7 days
 
 # Date range filter in sidebar using st.date_input
 start_date_input, end_date_input = st.sidebar.date_input(
