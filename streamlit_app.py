6431bde8a959da0ac5c15e047d2abbc751d35eb1
Tim Wywiol <tim-leon.wywiol.5831@student.uu.se>
2024-09-29 11:35:28 +0200

    💄add bee happy and icon as tab title in browswer

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ home_page = st.Page("app.py", title="Bee Health Monitoring", icon=":material/hom
 upload_events = st.Page("upload_events.py", title="Upload Events", icon=":material/upload:")
 
 pg = st.navigation([home_page, upload_events])
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+st.set_page_config(page_title="Bee Happy", page_icon=":bee:")
 pg.run()
