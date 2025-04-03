3eebc6fcb1be35f25b3e005550cbfe0d8dbd36df
Tim Wywiol <tim-leon.wywiol.5831@student.uu.se>
2025-04-03 21:24:11 +0200

    💄 Refactor data handling and add comparison tabs for beehive metrics

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 import streamlit as st
 import pandas as pd
 from datetime import timedelta
@@ -9,7 +10,7 @@ external_ip = get_external_ip()
 
 # Initialize session state variables if they don't exist
 if 'columns_to_plot' not in st.session_state:
-    st.session_state.columns_to_plot =  ['weight', 'temperature', 'humidity']
+    st.session_state.columns_to_plot =  ['weight']
 if 'start_date_input' not in st.session_state:
     st.session_state.start_date_input = None
 if 'end_date_input' not in st.session_state:
@@ -31,24 +32,23 @@ with st.expander("About this app"):
              "External IP:", external_ip)
 
 #"""Load raw data from MongoDB and CSV file and do some preprocessing"""
-data, rapid_weight_data = load_raw_data()
-data['timestamp'] = pd.to_datetime(data['timestamp'], format='mixed', yearfirst=True, utc=True)
-data['weight'] = pd.to_numeric(data['weight'], errors='coerce')
-data['temperature'] = pd.to_numeric(data['temperature'], errors='coerce')
-data['humidity'] = pd.to_numeric(data['humidity'], errors='coerce')
+raw_data, rapid_weight_data = load_raw_data()
+raw_data['timestamp'] = pd.to_datetime(raw_data['timestamp'], format='mixed', yearfirst=True, utc=True)
+raw_data['weight'] = pd.to_numeric(raw_data['weight'], errors='coerce')
+raw_data['temperature'] = pd.to_numeric(raw_data['temperature'], errors='coerce')
+raw_data['humidity'] = pd.to_numeric(raw_data['humidity'], errors='coerce')
 rapid_weight_data['created_at'] = pd.to_datetime(rapid_weight_data['created_at'])
 rapid_weight_data['end_date'] = pd.to_datetime(rapid_weight_data['end_date'])
 
 # Default date range is the last day of data
-end_date = data['timestamp'].max()
+end_date = raw_data['timestamp'].max()
 start_date = end_date - timedelta(days=30)
 
 #"""Sidebar components"""
 st.sidebar.title("Settings")
 ## Sidebar for selecting beehive_id in data
-beehive_ids = sorted(data['beehive_id'].unique())
-selected_beehive_id = st.sidebar.selectbox("Select Beehive ID",options=beehive_ids, key="selected_beehive_id")
-data = data[data['beehive_id'] == selected_beehive_id]
+beehive_ids = sorted(raw_data['beehive_id'].unique().tolist())
+st.sidebar.selectbox("Select Beehive ID",options=beehive_ids, key="selected_beehive_id")
 ## Sidebar for column selection
 columns_to_plot = st.sidebar.multiselect(
     "Select the columns to plot",
@@ -61,7 +61,7 @@ date_input = st.sidebar.date_input(
     "Select a date range",
     value=(st.session_state.start_date_input or start_date.date(),
            st.session_state.end_date_input or end_date.date()),
-    min_value=data['timestamp'].min().date(),
+    min_value=raw_data['timestamp'].min().date(),
     max_value= pd.to_datetime('today').date()+ timedelta(days=1),
     format="DD.MM.YYYY"
 )
@@ -79,61 +79,83 @@ start_date_input = pd.to_datetime(
 end_date_input = pd.to_datetime(end_date_input).tz_localize('Europe/Berlin')
 
 #'''FILTERING DATA'''
-filtered_data = data[(data['timestamp'] >= start_date_input)
-                     & (data['timestamp'] <= end_date_input + timedelta(days=1))]
+raw_data_date_range = raw_data[(raw_data['timestamp'] >= start_date_input)
+                     & (raw_data['timestamp'] <= end_date_input + timedelta(days=1))]
 rapid_weight_data_selected = rapid_weight_data[(rapid_weight_data['end_date'] >= start_date_input) & (
     rapid_weight_data['created_at'] <= end_date_input)]
-agg_data = filtered_data.groupby([pd.Grouper(key='timestamp', freq='D'), 'beehive_id']).agg({
+agg_data = raw_data_date_range.groupby([pd.Grouper(key='timestamp', freq='D'), 'beehive_id']).agg({
     'weight': 'mean',
     'temperature': 'mean',
     'humidity': 'mean'
 }).reset_index()
 
 col1, col2, col3 = st.columns(3)
-
+agg_data_selected_beehive = agg_data[agg_data['beehive_id'] == st.session_state.selected_beehive_id]
 with col1:
-    calculate_and_display_metric(agg_data,'weight', 'Weight (kg)',start_date_input,end_date_input, col1)
-    plot_line_chart(agg_data,'weight','green')
+    calculate_and_display_metric(agg_data_selected_beehive,'weight', 'Weight (kg)',start_date_input,end_date_input, col1)
+    plot_line_chart(agg_data_selected_beehive,'weight','green')
 with col2:
-    calculate_and_display_metric(agg_data,'temperature', 'Temp. (°C)',start_date_input,end_date_input, col2)
-    plot_line_chart(agg_data,'temperature','red')
+    calculate_and_display_metric(agg_data_selected_beehive,'temperature', 'Temp. (°C)',start_date_input,end_date_input, col2)
+    plot_line_chart(agg_data_selected_beehive,'temperature','red')
 with col3:
-    calculate_and_display_metric(agg_data,'humidity', 'Humidity (%)',start_date_input,end_date_input, col3)
-    plot_line_chart(agg_data,'humidity','blue')
+    calculate_and_display_metric(agg_data_selected_beehive,'humidity', 'Humidity (%)',start_date_input,end_date_input, col3)
+    plot_line_chart(agg_data_selected_beehive,'humidity','blue')
 
-# Plotting Raw Data section
-st.write(
-    f"#### Raw Data: {', '.join([col.capitalize() for col in columns_to_plot])} over the last 24 hours")
-st.write(f"<span style='color: grey;'>Latest timestamp: {data['timestamp'].max().strftime('%H:%M %d-%m-%Y')}</span>", unsafe_allow_html=True)
-fig = px.line(filtered_data[(filtered_data['timestamp'] >= end_date_input - timedelta(days=1))
-                     & (filtered_data['timestamp'] <= end_date_input + timedelta(days=1)) ].sort_values(by='timestamp'), x='timestamp',
-              y=columns_to_plot, line_shape='spline', color_discrete_sequence=['green','red','blue'])
+# Create different tabs
+tab1, tab2, tab3= st.tabs(["Raw Data","Beehive comparison", "Rapid Weight Changes"])
+with tab1:
+    raw_data_date_range_selected_beehive= raw_data_date_range[raw_data_date_range['beehive_id'] == st.session_state.selected_beehive_id]
+    st.write(
+        f"#### Raw Data: {', '.join([col.capitalize() for col in columns_to_plot])} over the last 24 hours")
+    st.write(f"<span style='color: grey;'>Latest timestamp: {raw_data['timestamp'].max().strftime('%H:%M %d-%m-%Y')}</span>", unsafe_allow_html=True)
+    fig = px.line(raw_data_date_range_selected_beehive[(raw_data_date_range_selected_beehive['timestamp'] >= end_date_input - timedelta(days=1))
+                        & (raw_data_date_range_selected_beehive['timestamp'] <= end_date_input + timedelta(days=1)) ].sort_values(by='timestamp'), x='timestamp',
+                y=columns_to_plot, line_shape='spline', color_discrete_sequence=['green','red','blue'])
 
-# Add intervals from rapid_weight_data_selected
-for _, row in rapid_weight_data_selected.iterrows():
-    fill_color = "green" if row['weight_diff'] > 0 else "red"
-    fig.add_vrect(
-        x0=row['created_at'], x1=row['end_date'],
-        fillcolor=fill_color, opacity=0.3, line_width=3, line_color=fill_color,
+    # Add intervals from rapid_weight_data_selected
+    for _, row in rapid_weight_data_selected.iterrows():
+        fill_color = "green" if row['weight_diff'] > 0 else "red"
+        fig.add_vrect(
+            x0=row['created_at'], x1=row['end_date'],
+            fillcolor=fill_color, opacity=0.3, line_width=3, line_color=fill_color,
+        )
+    st.plotly_chart(fig)
+    
+with tab2:
+    st.write(
+        f"#### {', '.join([col.capitalize() for col in columns_to_plot])} Comparison")
+    random_beehive_id = random.choice([beehive_id for beehive_id in beehive_ids if beehive_id != st.session_state.selected_beehive_id])
+    # Create a multi-select box for beehive IDs
+    selected_beehives = st.multiselect(
+        "Select Beehive IDs to compare",
+        options=beehive_ids,
+        default=[st.session_state.selected_beehive_id, random_beehive_id]
     )
-st.plotly_chart(fig)
+    if selected_beehives:
+        filtered_data_comparison = agg_data[agg_data['beehive_id'].isin(selected_beehives)]
+        fig = px.line(filtered_data_comparison, x='timestamp', y=columns_to_plot, color='beehive_id',
+                      line_shape='spline', color_discrete_sequence=['orange','purple','grey'])
+        st.plotly_chart(fig)
+    
+    
+    
+with tab3:
+    st.write("### Detection of rapid weight changes events")
+    # Calculate and display the metric for rapid weight changes
+    col1, col2, col3 = st.columns(3)
+    calculate_and_display_rapid_weight_changes(rapid_weight_data,end_date_input, col1)
+    st.write(rapid_weight_data_selected)
+    # Adjust start and end date to include the last hour of the day (23:59)
+    start_date_input = start_date_input.replace(
+        hour=0, minute=0, second=0, microsecond=0)
+    end_date_input = end_date_input.replace(
+        hour=23, minute=59, second=59, microsecond=999999)
 
-st.write("### Detection of rapid weight changes events")
-# Calculate and display the metric for rapid weight changes
-col1, col2, col3 = st.columns(3)
-calculate_and_display_rapid_weight_changes(rapid_weight_data,end_date_input, col1)
-st.write(rapid_weight_data_selected)
-# Adjust start and end date to include the last hour of the day (23:59)
-start_date_input = start_date_input.replace(
-    hour=0, minute=0, second=0, microsecond=0)
-end_date_input = end_date_input.replace(
-    hour=23, minute=59, second=59, microsecond=999999)
+    events_df = load_events(start_date_input, end_date_input)
 
-events_df = load_events(start_date_input, end_date_input)
-
-st.write("### Uploaded Events")
-if not events_df.empty:
-    st.write(events_df[['event_date', 'event_type',
-             'event_description', 'uploaded_image']])
-else:
-    st.write("No events found in the selected date range.")
+    st.write("### Uploaded Events")
+    if not events_df.empty:
+        st.write(events_df[['event_date', 'event_type',
+                'event_description', 'uploaded_image']])
+    else:
+        st.write("No events found in the selected date range.")
